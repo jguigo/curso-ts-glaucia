0ae95e56cdcce134c357b7c5c402573632c9b7d5
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-16 13:39:36 -0300

    numbers

--- Modulo-02/3-number-e-bigint/number-big.js
+++ Modulo-02/3-number-e-bigint/number-big.js
@@ -8,4 +8,12 @@ console.log(num1);
 console.log(num2);
 console.log(num3);
 console.log(num4);
-// => Exemplo bigint
+// => Exemplo bigint -> para usar tem que ser a versão target do es2020 ou ESNEXT
+let big1 = 90000004758456445665465456n;
+let big2 = 1n;
+let big3 = 0x200000000003n;
+let big4 = 2361183241434822606851n;
+console.log(big1);
+console.log(big2);
+console.log(big3);
+console.log(big4);
