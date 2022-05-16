70eeaeefcf1c7a9efd57772fd3f40392e8886d3f
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-16 13:39:43 -0300

    strings

---
+++ Modulo-02/4-string/string.js
@@ -0,0 +1,7 @@
+"use strict";
+let nomeCompleto = "Guilherme Melo";
+console.log(nomeCompleto);
+let funcaoEmpresa = "Estudante de Back end";
+console.log(funcaoEmpresa);
+let dadosFuncionario = `Seja bem vindo ${nomeCompleto}, ${funcaoEmpresa}`;
+console.log(dadosFuncionario);
---
+++ Modulo-02/4-string/string.ts
@@ -0,0 +1,8 @@
+let nomeCompleto: string = "Guilherme Melo";
+console.log(nomeCompleto);
+
+let funcaoEmpresa: string = "Estudante de Back end";
+console.log(funcaoEmpresa);
+
+let dadosFuncionario: string = `Seja bem vindo ${nomeCompleto}, ${funcaoEmpresa}`;
+console.log(dadosFuncionario);
